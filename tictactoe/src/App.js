e5309ef82a19b1d2a49a8e66565b42ed4c204533
youngpar <youngseo321@gmail.com>
2023-05-20 22:33:20 +0900

    tictactoe button

---
+++ tictactoe/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+
+//  export : Access from extern
+//  default : explicit declaration for main component
+//  return : return button
+export default function Square() {
+    return <button className="square">X</button>
+}
